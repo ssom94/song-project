786e194098d656e85700789a47b389c77f2123f8
ssom94 <sym94091jp@gmail.com>
2026-08-29 13:05:01 +0900

    Add local R2 PUT/GET test endpoint

--- src/index.ts
+++ src/index.ts
@@ -11,14 +11,51 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+const R2_TEST_KEY = 'test/hello.txt';
+const R2_TEST_VALUE = 'Hello R2';
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		const url = new URL(request.url);
+
 		switch (url.pathname) {
 			case '/message':
 				return new Response('Hello, World!');
+
 			case '/random':
 				return new Response(crypto.randomUUID());
+
+			case '/r2-test': {
+				if (request.method === 'PUT') {
+					await env.song_project_assets.put(R2_TEST_KEY, R2_TEST_VALUE, {
+						httpMetadata: {
+							contentType: 'text/plain; charset=utf-8',
+						},
+					});
+
+					return new Response(`Stored ${R2_TEST_KEY}`, { status: 201 });
+				}
+
+				if (request.method === 'GET') {
+					const object = await env.song_project_assets.get(R2_TEST_KEY);
+
+					if (object === null) {
+						return new Response('R2 test object not found', { status: 404 });
+					}
+
+					return new Response(object.body, {
+						headers: {
+							'Content-Type': object.httpMetadata?.contentType ?? 'text/plain; charset=utf-8',
+						},
+					});
+				}
+
+				return new Response('Method Not Allowed', {
+					status: 405,
+					headers: { Allow: 'GET, PUT' },
+				});
+			}
+
 			default:
 				return new Response('Not Found', { status: 404 });
 		}
